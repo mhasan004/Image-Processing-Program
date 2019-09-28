7fd334cd08b9baa096ccecb84341918cd7742f03
Mahmudul Hasan <mhasan0047@gmail.com>
2019-09-28 00:15:44 -0400

    clip

--- HW1/hw1/HW_clip.cpp
+++ HW1/hw1/HW_clip.cpp
@@ -20,13 +20,6 @@ HW_clip(ImagePtr I1, int t1, int t2, ImagePtr I2)
 	int h = I1->height();
 	int total = w * h;
 
-	// init lookup table: for all graylevels less than t1 -> LUT is t1. greather than t2 -> LUT is t2
-	//int i, lut[MXGRAY]; //MXGRAY = 256 (all graylevels 0 to 255)
-	//for (i = 0; i < t1 && i < MXGRAY; ++i) 
-	//	lut[i] = t1;
-	//for (i = 0; i > t2 && i < MXGRAY; ++i)
-	//	lut[i] = t2;
-
 	int lut[MXGRAY];
 	for (int i = 0; i < MXGRAY; ++i) {
 		if (i < t1)
