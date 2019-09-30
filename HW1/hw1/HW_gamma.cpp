461df96445f35f3ea816b02e5be81b5d4bc72557
Mahmudul Hasan <mhasan0047@gmail.com>
2019-09-30 15:40:14 -0400

    fixed gamma correction

--- HW1/hw1/HW_gamma.cpp
+++ HW1/hw1/HW_gamma.cpp
@@ -5,13 +5,12 @@ using namespace IP;
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // HW_gammaCorrect:
-//
 // Gamma correct image I1. Output is in I2.
-//
+
 void
 HW_gammaCorrect(ImagePtr I1, double gamma, ImagePtr I2)
 {
-	// s = c * r^gamma. c=1, r=image input
+	// s = c * (r/c)^gamma. c=1 usually but in this case it is 255, r=image input
 	// gamma correction: s = c * r^(1/gamma).
 
 	// copy image header (width, height) of input image I1 to output image I2
@@ -26,93 +25,20 @@ HW_gammaCorrect(ImagePtr I1, double gamma, ImagePtr I2)
 	int total = w * h;
 
 	// init lookup table
+	float lut[MXGRAY];
 	int c = 255;
-	int lut[MXGRAY];
-	for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) { // get input pointer for the channel 'ch' we are working on. This is the grayscale channel. so only one iteration  
-		for (int i = 0; i < MXGRAY; i++) {					  // LUT: generage an output grayscale value for every possible input gray scale [0,255]
-			// log moves the gamma * log(i +1); moves the graph as u go
-			int output = pow(i, 1/gamma);
-			//int output = pow(i, 1 / gamma)*log(i +1);
-			//int output = pow(i, 1/gamma)*log(i + 1);
-
-			if (output < 0) output = 0;
-			if (output > 255) output = 255;
-			lut[i] = (int)output;
-		}
+	for (int i = 0; i < MXGRAY; i++) {
+		float output = c * pow((float)i / c, 1 / gamma);
+		if (output < 0.0) output = 0.0;
+		if (output > 255.0) output = 255.0;
+		lut[i] = output;
 	}
 
 	// visit all image channels and evaluate output image
 	for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {	// get input  pointer for channel ch
 		IP_getChannel(I2, ch, p2, type);						// get output pointer for channel ch
 		for (int i = 0; i < total; i++)
-			*p2++ = lut[*p1++];			                        // use lut[] to eval output
+			*p2++ = lut[*p1++];									// use lut[] to eval output
 	}
 }
-
-//int output = pow(i, 1 / gamma)*log(i +1);
-
-//
-//// init lookup table
-//int c = 255;
-//int lut[MXGRAY];
-//for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) { // get input pointer for the channel 'ch' we are working on. This is the grayscale channel. so only one iteration  
-//	for (int i = 0; i < MXGRAY; i++) {					  // go throught image
-//		int output = pow(i, 1 / gamma);
-//
-//		if (output < 0) output = 0;
-//		if (output > 255) output = 255;
-//		lut[i] = (int)output;
-//	}
-//}
-//
-//// visit all image channels and evaluate output image
-//for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {	// get input  pointer for channel ch
-//	IP_getChannel(I2, ch, p2, type);						// get output pointer for channel ch
-//	for (int i = 0; i < total; i++)
-//		*p2++ = lut[*p1++];			// use lut[] to eval output
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//int c = 100;
-	//int lut[MXGRAY];
-	//for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) { // get input  pointer for channel ch ***(i think this returns the pointer that points to the first pixel in each row of pixels in the imgae or maybe each bit of a 8bit pixel)
-	//	IP_getChannel(I2, ch, p2, type);
-	//	for (int i = 0; i < total; i++) {					  // go throught image
-	//		int input = *p1++;
-	//		int output = (int) (gamma * pow(input, 1/gamma));
-	//		if (output < 0) output = 0;
-	//		if (output > 255) output = 255;
-
-	//		*p2++ = output;
-	//		  
-	//		
-	//	}
-	//}
-
-	// visit all image channels and evaluate output image
-	//for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {	// get input  pointer for channel ch
-	//	IP_getChannel(I2, ch, p2, type);	// get output pointer for channel ch
-	//	for (int i = 0; i < total; i++)
-	//		*p2++ = lut[*p1++];				// use lut[] to eval output
-	//}
-
-
 
