7173c7ade6b54f155fd36f8016ecfe7688f3b3d9
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2019-10-23 16:57:24 -0400

    hw_err_def_buff_done
    Error def party began

--- CS470.skel/hw2/HW_errDiffusion.cpp
+++ CS470.skel/hw2/HW_errDiffusion.cpp
@@ -29,4 +29,56 @@ using namespace IP;
 void
 HW_errDiffusion(ImagePtr I1, int method, bool serpentine, double gamma, ImagePtr I2)
 {
+
+
+
+
+
+}
+
+void copyRowToCircBuffer(ChannelPtr<uchar> p1, short* buffer, int w, int size) {
+	int i = 0;
+	for (i = 0; i < size / 2;    i++) {
+		buffer[i] = *p1; }
+    for (i = 0; i < size + w - 1; i++) {
+		buffer[i] = *p1;
+	}
+
+	for (i = 0; i < size / 2 + w - 1; i++) {
+		buffer[i] = *p1++;
+	}
+}
+
+void
+HW_gammaCorrect(ImagePtr I1, double gamma, ImagePtr I2)
+{
+	// s = c * r^gamma. c=1, r=image input
+	// gamma correction: s = c * r^(1/gamma).
+
+	// copy image header (width, height) of input image I1 to output image I2
+	IP_copyImageHeader(I1, I2);
+	// declarations for image channel pointers and datatype
+	ChannelPtr<uchar> p1, p2;
+	int type;
+
+	// init vars for width, height, and total number of pixels
+	int w = I1->width();
+	int h = I1->height();
+	int total = w * h;
+
+
+	// init lookup table
+
+	int i, lut[MXGRAY];
+	for (i = 0; i < MXGRAY; ++i) {
+		lut[i] = (int)CLIP((double)MaxGray*(pow((double)i / MaxGray, (1 / gamma))), 0, MaxGray);
+
+	}
+
+	// visit all image channels and evaluate output image
+	for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {	// get input  pointer for channel ch
+		IP_getChannel(I2, ch, p2, type);						// get output pointer for channel ch
+		for (int i = 0; i < total; i++)
+			*p2++ = lut[*p1++];			                        // use lut[] to eval output
+	}
 }
