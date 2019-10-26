fc149dc2c07b59ff238f0c4154f375be21025814
Mahmudul Hasan <mhasan0047@gmail.com>
2019-10-26 18:22:02 -0400

    blur x is commented, blur y is getting there

--- CS470.skel/hw2/HW_blur.cpp
+++ CS470.skel/hw2/HW_blur.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// HW_blur: Written By Mahmudul Hasan and Aninda
+// HW_blur: Written By mahmudul Hasan and Aninda
 //
 // Blur image I1 with a box filter (unweighted averaging).
 // The filter has width filterW and height filterH.
@@ -36,8 +36,8 @@ void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 	IP_copyImageHeader(I1, I2);
 	IP_copyImageHeader(I1, I3);														// copy I1 header files to I3
 
-	int w = I1->width();
-	int h = I1->height();
+	const int w = I1->width();
+	const int h = I1->height();
 	int total = w * h;
 
 	ChannelPtr<uchar> in, out, buffer;												// Image channel pointers and datatype
@@ -67,66 +67,111 @@ void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 	//DBOUT(L"\npaddingNumber_H:  ");
 	//DBOUT(paddingNumberH);
 
-	// x blur I1 to I3
+	
 	for (int ch = 0; IP_getChannel(I1, ch, in, type); ch++) 						// get input  pointer for channel ch
 	{
-		IP_getChannel(I2, ch, out, type);
-		for (int row = 0; row < h; ++row)
-		{																	
-			paddedBufferW.clear();
+		// VERTICAL Blur I3 to I2
+		// NEED TO PUT THE ORIGINAL IMAGE INTO A 2D VECTOR MATRIX TO ACESS VERTICLE STRIP[S WITH EASE. Dont want to constantly increment pixel pointers, makes it slow 
+		// 1) making a h x w 2D vector matrix of all zeroes
+		vector<vector<int> > copy(h);
+		for (int i = 0; i < h; i++)
+			copy[i].resize(w);
 
-			int count = -1;
-			for (int col = 0; col < w; ++col)										// 1) Put the input row into a buffer vector and add appropiate padding on sides
+		// 2) copy data to vector matrix
+		IP_getChannel(I1, ch, in, type);
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < h; j++)
+				copy[i][j] = *in++;
+
+		int rowNumber;
+		IP_getChannel(I1, ch, in, type);
+		for (int col = 0; col < w; ++col)
+		{
+			paddedBufferH.clear();
+			for (int row = 0; row < h; ++row)
 			{
-				count++;
-				int pixel = *in++;													// ***NOTE*** if i didnt use pixel = *in++ and just used *p++, the image gets cut diagonally where the left side in on the right and the right on the left. it was weird
-				if (col == 0) {														// 1a) if its the first pixel in the row...																		
-					for (int k = 0; k < paddingNumberW; ++k) 						// 1b) put zero paddings into the buffer 
-						paddedBufferW.push_back(pixel);
-					paddedBufferW.push_back(pixel);									// 1c) after padding, insert the first pixel of input row into buffer and copy row pixels as normal
+				rowNumber = row;
+				//for (int colCorr = 0; colCorr <= col; ++colCorr)					//column offset correction
+				//	*in++;
+				//for (int rowCorr = 0; rowCorr <= row * w; ++rowCorr)
+				//	pixel = *in++;
+				int pixel = copy[row][col];											//3) going throw the image vertically. so copy the pixel and use the buffer vector			
+				if (row == 0) {
+					for (int k = 0; k < paddingNumberH; ++k)
+						paddedBufferH.push_back(pixel);
+					paddedBufferH.push_back(pixel);
 				}
-				else if (col == w - 1) {											// 1d) at the end of the row add the last pixel to the buffer then add the padding to the buffer			
-					paddedBufferW.push_back(pixel);
-					for (int k = 0; k < paddingNumberW; ++k) {
-						paddedBufferW.push_back(pixel);
-					}
+				else if (row == h - 1) {
+					paddedBufferH.push_back(pixel);
+					for (int k = 0; k < paddingNumberH; ++k) 
+						paddedBufferH.push_back(pixel);
 				}
-				else {																// 1c) if not pixel at beginning or end of image, just copy pixels
-					paddedBufferW.push_back(pixel);
-				}
-				/*if ((row==0) && (col == 0 || col == 1 || col == w - 2 || col == w - 1)) {
-					DBOUT(L"\nCol: ");
-					DBOUT(col);
-					DBOUT(L"\n     Vector_Size: ");
-					DBOUT(paddedBufferW.size());
-				}*/
+				else 
+					paddedBufferH.push_back(pixel);
 			}
-			/*if (row == 0) {
-				DBOUT(L"\n\nCol Count: ");
-				DBOUT(count);
 
-				DBOUT(L"\n\nPrinting row 0 vector:\n");
-				for (int i = 0; i < paddedBufferW.size(); ++i) {
-					DBOUT(paddedBufferW.at(i));
-					DBOUT(L" ");
-				}
-			}*/
-			
-																					//2 and 3) now that i recorded the rows need to blur the row using the filter and output each blured pixel to output
-			int countera = 0;
-			for (int window = paddingNumberW; window < (paddedBufferW.size() - paddingNumberW); ++window) {
+			for (int window = paddingNumberH; window < (paddedBufferH.size() - paddingNumberH); ++window) 
+			{
 				int sum = 0;
-				sum += paddedBufferW.at(window);								//the cause of so much headache. at(window)
-				for (int i = 1; i <= paddingNumberW; ++i) {
-					sum += paddedBufferW.at(window - i);
-					sum += paddedBufferW.at(window + i);
+				sum += paddedBufferH.at(window);
+				for (int i = 1; i <= paddingNumberH; ++i) {
+					sum += paddedBufferH.at(window - i);
+					sum += paddedBufferH.at(window + i);
 				}
-				*out++ = CLIP(sum/filterW, 0, 255);									//3) print blured pixel to buffer	
-				countera++;
+				//overwrite copy matrix with result:										//4) currently averaging window. output the result and overwrite the pixel at copy matrix. after finishing copy matrix, will print to output
+				copy[window-paddingNumberH][col] = CLIP(sum / filterW, 0, 255);
+
+
+				//Find the correct position of the pixel for vertical offseet. For Horizontal didnt need to do this. For vertical im doign columns first so i need to push the outpixel appropiately
+				/*IP_getChannel(I2, ch, out, type);							//column correction, get output pixel pointer to the right column
+				for (int colCorr = 0; colCorr <= col; ++colCorr)
+					*out++;
+				for (int rowCorr = 0; rowCorr <= rowNumber * w; ++rowCorr)
+					*out++;
+				*out++ = CLIP(sum / filterW, 0, 255);*/
 			}
-			/*if (row == 0) {
-				DBOUT(L"\nNumber of filter moves:  ");
-				DBOUT(countera);
-			}*/
 		}
+		//5) finally copy matrix to output
+		IP_getChannel(I2, ch, buffer, type);
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < h; j++)
+				*buffer++ = copy[i][j];
+
+
+
+		////// Horizontal Blurring: I1 to I3
+		//IP_getChannel(I3, ch, buffer, type);
+		//for (int row = 0; row < h; ++row)
+		//{																	
+		//	paddedBufferW.clear();
+		//	IP_getChannel(I2, ch, out, type);
+		//	for (int col = 0; col < w; ++col)										// 1) Put the input row into a buffer vector and add appropiate padding on sides
+		//	{
+		//		int pixel = *in++;													// ***NOTE*** if i didnt use pixel = *in++ and just used *p++, the image gets cut diagonally where the left side in on the right and the right on the left. it was weird
+		//		if (col == 0) {														// 1a) if its the first pixel in the row...																		
+		//			for (int k = 0; k < paddingNumberW; ++k) 						// 1b) put zero paddings into the buffer 
+		//				paddedBufferW.push_back(pixel);
+		//			paddedBufferW.push_back(pixel);									// 1c) after padding, insert the first pixel of input row into buffer and copy row pixels as normal
+		//		}
+		//		else if (col == w - 1) {											// 1d) at the end of the row add the last pixel to the buffer then add the padding to the buffer			
+		//			paddedBufferW.push_back(pixel);
+		//			for (int k = 0; k < paddingNumberW; ++k) {
+		//				paddedBufferW.push_back(pixel);
+		//			}
+		//		}
+		//		else {																// 1c) if not pixel at beginning or end of image, just copy pixels
+		//			paddedBufferW.push_back(pixel);
+		//		}
+		//	}															
+		//	for (int window = paddingNumberW; window < (paddedBufferW.size() - paddingNumberW); ++window) { //2 and 3) now that i recorded the rows need to blur the row using the filter and output each blured pixel to output
+		//		int sum = 0;
+		//		sum += paddedBufferW.at(window);										//the cause of so much headache. at(window)
+		//		for (int i = 1; i <= paddingNumberW; ++i) {
+		//			sum += paddedBufferW.at(window - i);
+		//			sum += paddedBufferW.at(window + i);
+		//		}
+		//		*out++ = CLIP(sum/filterW, 0, 255);									//3) print blured pixel to buffer	
+		//	}
+		//}
+
 	}
