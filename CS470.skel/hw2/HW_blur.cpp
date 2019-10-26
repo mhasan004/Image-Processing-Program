b1a7c2b39375a2377cb7031133fb2c40c9883375
Mahmudul Hasan <mhasan0047@gmail.com>
2019-10-26 04:33:04 -0400

    Workign x blur, now to y blur

--- CS470.skel/hw2/HW_blur.cpp
+++ CS470.skel/hw2/HW_blur.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// HW_blur: Written By mahmudul Hasan and Aninda
+// HW_blur: Written By Mahmudul Hasan and Aninda
 //
 // Blur image I1 with a box filter (unweighted averaging).
 // The filter has width filterW and height filterH.
@@ -40,38 +40,39 @@ void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 	int h = I1->height();
 	int total = w * h;
 
-	ChannelPtr<uchar> in, out, buffer;												// image channel pointers and datatype
+	ChannelPtr<uchar> in, out, buffer;												// Image channel pointers and datatype
 	int type;
 
-	// Bluring causes smaller image so i will pad the image with zeroes. 1padd for 3pixel windows, 2padd for 6
-//if (filterH % 2 == 0 && filterH!=1) {											// bluring window must be odd so will just subtract to previous window
-//	filterH--;
-//}
-//if (filterW % 2 == 0 && filterW != 1) {											// bluring window must be odd so will just subtract to previous window
-//	filterW--;
-//}
-	int paddingNumberH = (filterH - 1) / 2;											// number of padding needed on sides of rows for a given filter size
+	if (filterH % 2 == 0 && filterH!=1) {											// Bluring window must be odd so that the averaged of pixel in window goes to middle pxl. WITH OUT THIS: THE PICTURE GETS DARKER EVERY EVEN FILTER SIZE!
+		filterH--;
+	}
+	if (filterW % 2 == 0 && filterW != 1) {											
+		filterW--;
+	}
+																					// Bluring causes smaller image so i will pad the image with zeroes. 1padd for 3pixel windows, 2padd for 6
+	int paddingNumberH = (filterH - 1) / 2;											// Number of padding needed on sides of rows for a given filter size
 	int paddingNumberW = (filterW - 1) / 2;
-	vector<int> paddedBufferW;														// the buffer vector to shore each row with appropiate pading to left or right. Will then blur row and output to output image I2
+	vector<int> paddedBufferW;														// The buffer vector to shore each row with appropiate pading to left or right. Will then blur row and output to output image I2
 	vector<int> paddedBufferH;
-	DBOUT(L"\nw: ");
-	DBOUT(w);
-	DBOUT(L"\nh: ");
-	DBOUT(h);
-	DBOUT(L"\nfilter_W:    ");
-	DBOUT(filterW);
-	DBOUT(L"\nfilter_H:    ");
-	DBOUT(filterH);
-	DBOUT(L"\npaddingNumber_W:  ");
-	DBOUT(paddingNumberW);
-	DBOUT(L"\npaddingNumber_H:  ");
-	DBOUT(paddingNumberH);
-	// x blure I1 to I3
+	//DBOUT(L"\nw: ");
+	//DBOUT(w);
+	//DBOUT(L"\nh: ");
+	//DBOUT(h);
+	//DBOUT(L"\nfilter_W:    ");
+	//DBOUT(filterW);
+	//DBOUT(L"\nfilter_H:    ");
+	//DBOUT(filterH);
+	//DBOUT(L"\npaddingNumber_W:  ");
+	//DBOUT(paddingNumberW);
+	//DBOUT(L"\npaddingNumber_H:  ");
+	//DBOUT(paddingNumberH);
+
+	// x blur I1 to I3
 	for (int ch = 0; IP_getChannel(I1, ch, in, type); ch++) 						// get input  pointer for channel ch
 	{
 		IP_getChannel(I2, ch, out, type);
 		for (int row = 0; row < h; ++row)
-		{
+		{																	
 			paddedBufferW.clear();
 
 			int count = -1;
@@ -93,14 +94,14 @@ void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 				else {																// 1c) if not pixel at beginning or end of image, just copy pixels
 					paddedBufferW.push_back(pixel);
 				}
-				if ((row == 0) && (col == 0 || col == 1 || col == w - 2 || col == w - 1)) {
+				/*if ((row==0) && (col == 0 || col == 1 || col == w - 2 || col == w - 1)) {
 					DBOUT(L"\nCol: ");
 					DBOUT(col);
 					DBOUT(L"\n     Vector_Size: ");
 					DBOUT(paddedBufferW.size());
-				}
+				}*/
 			}
-			if (row == 0) {
+			/*if (row == 0) {
 				DBOUT(L"\n\nCol Count: ");
 				DBOUT(count);
 
@@ -109,24 +110,23 @@ void HW_blur(ImagePtr I1, int filterW, int filterH, ImagePtr I2)
 					DBOUT(paddedBufferW.at(i));
 					DBOUT(L" ");
 				}
-
-			}
-
-			//2 and 3) now that i recorded the rows need to blur the row using the filter and output each blured pixel to output
+			}*/
+			
+																					//2 and 3) now that i recorded the rows need to blur the row using the filter and output each blured pixel to output
 			int countera = 0;
 			for (int window = paddingNumberW; window < (paddedBufferW.size() - paddingNumberW); ++window) {
 				int sum = 0;
 				sum += paddedBufferW.at(window);								//the cause of so much headache. at(window)
-				for (int i = 0; i < paddingNumberW; ++i) {
+				for (int i = 1; i <= paddingNumberW; ++i) {
 					sum += paddedBufferW.at(window - i);
 					sum += paddedBufferW.at(window + i);
 				}
-				*out++ = CLIP(sum / filterW, 0, 255);									//3) print blured pixel to buffer	
+				*out++ = CLIP(sum/filterW, 0, 255);									//3) print blured pixel to buffer	
 				countera++;
 			}
-			if (row == 0) {
+			/*if (row == 0) {
 				DBOUT(L"\nNumber of filter moves:  ");
 				DBOUT(countera);
-			}
+			}*/
 		}
 	}
