f47e55e02e5e9c06c4bd394a0fe047d9ca876b2b
Mahmudul Hasan <mhasan0047@gmail.com>
2019-10-30 23:13:18 -0400

    median its sort of shorting a pic of some weight lines

--- CS470.skel/hw2/HW_median.cpp
+++ CS470.skel/hw2/HW_median.cpp
@@ -13,20 +13,23 @@ using std::vector;
 //
 
 //copy row a in vector A to row b in the same vector A
-void copyVectorRow(vector<vector<int> > &v, int a, int b) { //copy row a to row b
+void copyVectorRow(vector<vector<short> > &v, int a, int b) { //copy row a to row b
 	v[b].clear();
 	for (int i = 0; i < v[a].size(); ++i)
 		v[b].push_back(v[a].at(i));
 }
-void copyVectorRow(vector<vector<int> > &copyFrom, vector<vector<int> > &copyTo, int fromRow, int toRow) { //copy row a from vector a to row b of vector b
+void copyVectorRow(vector<vector<short> > &copyFrom, vector<vector<short> > &copyTo, int fromRow, int toRow) { //copy row a from vector a to row b of vector b
 	copyTo[toRow].clear();
 	for (int fromCol = 0; fromCol < copyFrom[fromRow].size(); ++fromCol)
 		copyTo[toRow].push_back(copyFrom[fromRow].at(fromCol));
 }
 
+
 void HW_median(ImagePtr I1, int filter, ImagePtr I2)
 {
 	if (filter < 9) filter = 9;
+	if (filter % 2 == 0) filter++;                                                               //keep filter odd
+	int k = 0; //what numbers to add from medium
 	IP_copyImageHeader(I1, I2);
 	ChannelPtr<uchar> in, out;																// Image channel pointers and datatype
 	int type;
@@ -34,14 +37,10 @@ void HW_median(ImagePtr I1, int filter, ImagePtr I2)
 	const int h = I1->height();
 	int total = w * h;
 
-	if (filter % 2 == 0 && filter != 1) 													// Bluring window must be odd so that the averaged of pixel in window goes to middle pxl. WITH OUT THIS: THE PICTURE GETS DARKER EVERY EVEN FILTER SIZE!
-		filter--;
-
 	int paddingNumber = (filter - 1) / 2;													// Number of padding needed on sides of rows for a given filter size
-	int bufferW = w + 2 * w;
 
-	vector<vector<int> > buff(filter);														// making a 2d vector matrix that can only have 'filter' rows. This will be the circular buffer. will just overwrite old rows.
-	vector<vector<int> > paddedImage(h + 2 * paddingNumber);
+	//vector<vector<int> > buff(filter);														// making a 2d vector matrix that can only have 'filter' rows. This will be the circular buffer. will just overwrite old rows.
+	vector<vector<short> > paddedImage(h + 2 * paddingNumber);
 
 	for (int ch = 0; IP_getChannel(I1, ch, in, type); ch++)
 	{
@@ -82,43 +81,122 @@ void HW_median(ImagePtr I1, int filter, ImagePtr I2)
 			}
 		}
 
-
 		//2) Circular buffer: initial buffer setup: add the first 'filter' number of rows from the Padded Image to the buffer.
-		for (int bIndex = 0; bIndex < filter; ++bIndex)
+		/*for (int bIndex = 0; bIndex < filter; ++bIndex)
 			for (int col = 0; col < paddedImage[bIndex].size(); ++col)
-				buff[bIndex].push_back(paddedImage[bIndex].at(col));
+				buff[bIndex].push_back(paddedImage[bIndex].at(col));*/
+		short **buff = new short*[filter];								  //***dynamic array wont let me do short *buff[filter];	
+		short **startOfBufferRow = new short*[filter];
+		short **startOfKernal = new short*[filter];
+		for (int bIndex = 0; bIndex < filter; ++bIndex) {
+			buff[bIndex] = paddedImage[bIndex].data();
+			startOfBufferRow[bIndex] = buff[bIndex];                      //***Set the reset pointer  
+			startOfKernal[bIndex] = buff[bIndex];
+		}
 
 		//3) As the filter increments down, replace the oldest row index with the newer row. Ex: filter 3x3: buff[0,1,2] -> window down -> buff[3,1,2] -> window down -> buff[3,4,2] -> window down -> buff[3,4,5] -> window down -> buff[6,4,5]
 		int medianIndex = filter * filter / 2;
-		int k = 1; //what numbers to add from medium
 		for (int yIncrement = 0; yIncrement < h; ++yIncrement)												// Sliding filter windows vertically direction
 		{
 			for (int xIncrement = 0; xIncrement < w; ++xIncrement)											// Sliding filter windows horizontally direction
 			{
-				vector<int> sorted;
+				vector<short> sorted;
 				int count = 1;
 				int sum = 0;
 
-				// A1) buffer is filled atm with the correct rows so add everything to the 'sorted' 1D vector and then sort the vector
-				for (int buffRow = 0; buffRow < filter; ++buffRow)											// Going through rows of buffer 9shifted the rows at the end)
-					for (int buffCol = xIncrement; buffCol < filter + xIncrement; ++buffCol)					// Going through cols of buffer (using the shift from the for loop)
-						sorted.push_back(buff[buffRow].at(buffCol));
-				sort(sorted.begin(), sorted.end());                                 //sort cevotr of the window
 
-				// A2) add the median values with the k val from left to right
+
+
+				//// A1) buffer is filled atm with the correct rows so add everything to the 'sorted' 1D vector and then sort the vector
+				//for (int buffRow = 0; buffRow < filter; ++buffRow)											// Going through rows of buffer 9shifted the rows at the end)
+				//	for (int buffCol = xIncrement; buffCol < filter + xIncrement; ++buffCol)					// Going through cols of buffer (using the shift from the for loop)
+				//		sorted.push_back(buff[buffRow].at(buffCol));
+				//sort(sorted.begin(), sorted.end());                                 //sort cevotr of the window
+
+				//// A2) add the median values with the k val from left to right
+				//sum = sum + sorted[medianIndex];
+				//for (int i = 1; i < k; ++i)
+				//{
+				//	count += 2;
+				//	sum = sum + sorted[medianIndex - i];
+				//	sum = sum + sorted[medianIndex + i];
+
+				//}
+				////*out++ = CLIP(sum / count, 0, 255);
+
+
+		
+				
+				
+				
+				//get the kernal int the sorted vector to be sorted
+				if (xIncrement == 0)                //**                  A1) buffer is filled atm with the correct rows so add everything to the 'sorted' 1D vector and then sort the vector
+				{
+					for (int buffRow = 0; buffRow < filter; ++buffRow)											// Going through rows of buffer 9shifted the rows at the end)
+					{
+						for (int buffCol = xIncrement; buffCol < filter + xIncrement; ++buffCol)					// Going through cols of buffer (using the shift from the for loop)
+						{
+							if (buffCol == (xIncrement + 1)) {                                     //***now i know where the first pixel of kernal will ne located in
+								startOfBufferRow[buffRow] = buff[buffRow];
+							}
+							sorted.push_back(*buff[buffRow]++);
+						}
+					}
+				}
+
+				else
+				{
+					for (int buffRow = 0; buffRow < filter; ++buffRow)											// Going through rows of buffer 9shifted the rows at the end)
+					{
+						for (int buffCol = xIncrement; buffCol < filter + xIncrement; ++buffCol)					// Going through cols of buffer (using the shift from the for loop)
+						{
+							if (buffCol == xIncrement)
+								buff[buffRow] = startOfBufferRow[buffRow];                                  //***get back the position of the first pixel of kernal row
+							if (buffCol == (xIncrement + 1))                                            //***set pos for the next iteration!
+								startOfBufferRow[buffRow] = buff[buffRow];
+							sorted.push_back(*buff[buffRow]++);
+						}
+					}
+				}
+				sort(sorted.begin(), sorted.end());
+
 				sum = sum + sorted[medianIndex];
 				for (int i = 1; i < k; ++i)
 				{
 					count += 2;
 					sum = sum + sorted[medianIndex - i];
 					sum = sum + sorted[medianIndex + i];
-
 				}
-				//*out++ = CLIP(sum / count, 0, 255);
+				//output[yIncrement].push_back(sum / count);
+				*out++ = CLIP(sum / count, 0, 255);
+
+
+			
+			
+			
+			
+			
+
+
+
+
+
+
+
+
+			
+			
+			
+			
+			
+			
 			}
 			//B) replace old row in buffer with the next row in opaddedImage
 			if (yIncrement < h - 1) {
-				copyVectorRow(paddedImage, buff, filter + yIncrement, yIncrement%filter); //possible fix
+				//copyVectorRow(paddedImage, buff, filter + yIncrement, yIncrement%filter); //possible fix
+				buff[yIncrement%filter] = paddedImage[filter + yIncrement].data();       //swaping the old row in buffer to the next row in the image
+				buff[(yIncrement + 1) % filter] = paddedImage[filter + yIncrement].data();
+				buff[(yIncrement + 2) % filter] = paddedImage[filter + yIncrement].data();
 			}
 		}
 
