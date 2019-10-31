5fd53801aeaf77d006d6ef4e9ef0a0dd4cd7d31e
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2019-10-31 00:12:56 -0400

    Update HW_convolve.cpp

--- CS470.skel/hw2/HW_convolve.cpp
+++ CS470.skel/hw2/HW_convolve.cpp
@@ -15,70 +15,73 @@ void copyRowTo_Buffer(ChannelPtr<uchar>, short*, int, int);
 
 void HW_convolve(ImagePtr I1, ImagePtr Ikernel, ImagePtr I2)
 {
+	int filter = Ikernel->width();
+	// if(filter % 2 == 0) filter++; // set filter to always be an odd number
+
 	IP_copyImageHeader(I1, I2);
 	int w = I1->width();
 	int h = I1->height();
 	int total = w * h;
+
+	int type;
 	ChannelPtr<uchar> input, out;
 	ChannelPtr<uchar> point;
-	
-	
-	int type;
-	int filter = Ikernel->width();
+
 	if (filter == 1) {
 		for (int ch = 0; IP_getChannel(I1, ch, input, type); ch++) {
 			IP_getChannel(I2, ch, out, type);
-			for (int i = 0; i<total ; i++) *out++ = *input++;
+			for (point = input + total; input < point;) {
+				*out++ = *input++;
+			}
 		}
 	}
 	else if (filter > 1) {
 
-		ChannelPtr<float> pKernel; 
-		IP_getChannel(Ikernel, 0, pKernel, type);
+		ChannelPtr<float> pointerKernl; // kernel value pointer
+		IP_getChannel(Ikernel, 0, pointerKernl, type);
 
-		int padded_bufsize = filter + w - 1; 
-		
-	    // Couldn't dynamically allocate
-		short* buffers[30]; 
-		for (int i = 0; i < filter; i++)
-		{
-		buffers[i] = new short[padded_bufsize];
-		}
-		
+		int bufSz = filter + w - 1;
+		short* buffers[12]; // array of 12 pointers , failed to dynamically allocate
+		for (int i = 0; i < filter; i++) buffers[i] = new short[bufSz];
+
 		for (int ch = 0; IP_getChannel(I1, ch, input, type); ch++) {
 			IP_getChannel(I2, ch, out, type);
-			point = input + total; // pointing to the total + input
+			point = input + total;
 
-			// Here copying the top padded row 1 to buffer.
+			// copying padded row1 to first buffers, 
 			for (int i = 0; i < filter / 2; i++)
 			{
 				copyRowTo_Buffer(input, buffers[i], w, filter);
 			}
-			
+			// finish the rest of buffers
+
+
+
 			for (int i = filter / 2; i < filter; i++) {
 				copyRowTo_Buffer(input, buffers[i], w, filter);
 				if (input < point - w) {
-					input = input + w;
+					input += w;
 				}
-				}
+			}
 
-			for (int col = 0; col < h; col++) {  
-				for (int r = 0; r < w; r++) {
+			for (int y = 0; y < h; y++) {  // visit each row
+				// visit each pixel in row
+				for (int x = 0; x < w; x++) {
 					float sum = 0.0;
 					for (int i = 0; i < filter; i++) {
 						for (int j = 0; j < filter; j++) {
-							sum += (buffers[i][j + r] * *pKernel++);
+							sum += (buffers[i][j + x] * *pointerKernl++);
 						}
 					}
-					pKernel -= filter * filter;
+					pointerKernl -= filter * filter;
 					*out++ = (int)CLIP(sum, 0, 255);
 				}
 
-				// circular quque
-				// remove buffers[0] and push all other buffers up by one index
+				
+			
 				for (int i = 0; i < filter - 1; i++) {
-					for (int j = 0; j < padded_bufsize; j++) {
-						buffers[i][j] = buffers[i + 1][j];
+					for (int j = 0; j < bufSz; j++) {
+						buffers[i][j] = buffers[i + 1][j];	// remove buffers[0] and push all other buffers up by one index
 					}
 				}
 				// copy next row to last buffer
@@ -86,12 +89,10 @@ void HW_convolve(ImagePtr I1, ImagePtr Ikernel, ImagePtr I2)
 				if (input < point - w) input += w;
 			}
 		}
-	
-		for (int i = 0; i < 30; i++) delete[] buffers[i];
-	
+		for (int i = 0; i < filter; i++) delete[] buffers[i];
 	}
 	
-
+	
 }
 
 void copyRowTo_Buffer(ChannelPtr<uchar> in, short* buffer, int w, int size) {
